c8f82d6b8f7a0251c2f3f55a490b304a095bd6c3
hany-7878 <htesfaye641@gmail.com>
2025-11-03 01:29:40 -0800

    set up done

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Card from '../components/Card'
+import Button from '../components/Button'
+import { PLACEHOLDER_IMAGE } from '../constants'
+
+export default function Home() {
+  return (
+    <div className="p-8 ">
+      <h1 className="text-2xl font-bold mb-6">ALX Listing App</h1>
+      <Card
+        title="Beautiful House"
+        description="A lovely house in the city center"
+        image={PLACEHOLDER_IMAGE}
+      />
+      <div className="mt-4">
+        <Button label="Book Now" onClick={() => alert('Booked!')} />
+      </div>
+    </div>
+  )
+}
